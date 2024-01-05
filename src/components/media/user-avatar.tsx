03b847c7f2dfcd038609146ccf4b0218e2ea8a7c
William Fernandes Dias <william.winchester1967@gmail.com>
2024-01-05 19:43:33 -0300

    feat: implementar componente para mostrar informações sobre o usuário logado nas telas

---
+++ src/components/media/user-avatar.tsx
@@ -0,0 +1,34 @@
+import {
+  Avatar,
+  AvatarFallbackText,
+  AvatarImage,
+  Icon,
+} from "@gluestack-ui/themed";
+import { User } from "firebase/auth";
+import { User as UserIcon } from "lucide-react-native";
+
+type UserAvatarProps = {
+  user: User | null;
+};
+
+export default function UserAvatar({ user = null }: UserAvatarProps) {
+  return (
+    <Avatar size="md">
+      {user ? (
+        <>
+          <AvatarFallbackText>{user.displayName}</AvatarFallbackText>
+          {user.photoURL && (
+            <AvatarImage
+              source={{
+                uri: user.photoURL,
+              }}
+              alt={user.displayName || "Usuário"}
+            />
+          )}
+        </>
+      ) : (
+        <Icon as={() => <UserIcon size={24} />} />
+      )}
+    </Avatar>
+  );
+}
