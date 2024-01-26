8ee6258dfb7e06429a8456c3de32df78bd48ee0b
William Fernandes Dias <william.winchester1967@gmail.com>
2024-01-26 16:08:26 -0300

    feat: implementar hoc para adicionar modal para dar feedback sobre erros ao usuário nas páginas

--- src/hooks/use-error-modal.tsx
+++ src/hooks/use-error-modal.tsx
@@ -1,7 +1,19 @@
 import ErrorModal from "@/components/overlay/error-modal";
-import { type ReactNode, useEffect, useState } from "react";
+import {
+  type ComponentProps,
+  type ComponentType,
+  type JSXElementConstructor,
+  type ReactNode,
+  useEffect,
+  useState,
+} from "react";
 
 export type UseErrorModalReturn = ReactNode;
+
+export type ErrorModalWrapperProps<
+  P,
+  C extends keyof JSX.IntrinsicElements | JSXElementConstructor<P>,
+> = ComponentProps<C> & { onError: (error: Errors.IError) => void };
 
 /**
  * Retorna um modal descrevendo o erro. O modal é exibido quando o erro não for nulo.
@@ -23,4 +35,32 @@ export function useErrorModal(
   }, [error]);
 
   return error && <ErrorModal error={error} isOpen={show} onClose={onClose} />;
+}
+
+/**
+ * Adiciona um modal de erro ao componente, que é exibido ao usuário quando a
+ * callback na propriedade `onError` é chamada.
+ *
+ * @param {ComponentType} Component Componente a ser envolvido
+ * @returns Componente envolvido com o modal de erro
+ */
+export function withErrorModal<
+  T extends { onError: (error: Errors.IError) => void },
+>(Component: ComponentType<T>) {
+  return function ErrorModalWrapper(
+    props: ErrorModalWrapperProps<T, ComponentType<T>>,
+  ): ReturnType<typeof useErrorModal> {
+    const [error, setError] = useState<Errors.IError | null>(null);
+
+    function onClose() {
+      setError(null);
+    }
+
+    return (
+      <>
+        <Component {...props} onError={setError} />
+        {useErrorModal(error, onClose)}
+      </>
+    );
+  };
 }
