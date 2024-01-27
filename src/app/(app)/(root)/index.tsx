2f0bee624ba9575e44083df2b53673fd9b76869c
William Fernandes Dias <william.winchester1967@gmail.com>
2024-01-27 10:07:18 -0300

    feat: implementar página inicial da dashboard, onde o usuário conecta o aplicativo a um dos robôs

--- src/app/(app)/(root)/index.tsx
+++ src/app/(app)/(root)/index.tsx
@@ -1,5 +1,15 @@
+import ConnectBluetoothButton from "@/components/forms/connect-bluetooth-button";
 import Page from "@/components/layout/page";
+import { withErrorModal } from "@/hooks/use-error-modal";
+import { router } from "expo-router";
 
-export default function IndexScreen() {
-  return <Page />;
-}
+export default withErrorModal(function IndexScreen({ onError }) {
+  return (
+    <Page alignItems="center" justifyContent="center">
+      <ConnectBluetoothButton
+        onConnect={() => router.push("/(app)/(root)/robot/")}
+        onError={onError}
+      />
+    </Page>
+  );
+});
