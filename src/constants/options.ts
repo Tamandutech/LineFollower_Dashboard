9d5a17f403ae0c10530303a3aaaa013f4d2b1709
William Fernandes Dias <william.winchester1967@gmail.com>
2024-02-06 22:15:36 -0300

    refactor: mover constantes para um diretório próprio

---
+++ src/constants/options.ts
@@ -0,0 +1,39 @@
+import { batteryVoltageFormatter } from "@/models/battery";
+import { defaultSettings } from "@/models/sessions";
+import { ONE_MINUTE_IN_MILLISECONDS } from ".";
+
+export type Option = {
+  value: number;
+  label: string;
+};
+
+export const batteryStatusUpdateIntervalOptions: Array<Option> = Array.from(
+  new Set([
+    0,
+    30000,
+    60000,
+    90000,
+    120000,
+    defaultSettings.batteryStatusUpdateInterval,
+  ]),
+).map((interval) => ({
+  value: interval,
+  label: `${interval / ONE_MINUTE_IN_MILLISECONDS} ${
+    interval > ONE_MINUTE_IN_MILLISECONDS ? "minutos" : "minuto"
+  }`,
+}));
+
+export const batteryLowWarningThresholdOptions: Array<Option> = Array.from(
+  new Set([
+    7900,
+    7600,
+    7400,
+    7200,
+    6900,
+    6600,
+    defaultSettings.batteryLowWarningThreshold,
+  ]),
+).map((threshold) => ({
+  label: `${batteryVoltageFormatter.format(threshold / 1000)}V`,
+  value: threshold,
+}));
