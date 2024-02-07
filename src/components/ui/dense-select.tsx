fc6538cfcb285e48da20e88165a4e034b6747b97
William Fernandes Dias <william.winchester1967@gmail.com>
2024-02-06 22:14:57 -0300

    feat: implementar componente para seleção de opções com uma interface mais minimalista

---
+++ src/components/ui/dense-select.tsx
@@ -0,0 +1,64 @@
+import {
+  ChevronRightIcon,
+  Icon,
+  Pressable,
+  Select,
+  SelectBackdrop,
+  SelectContent,
+  SelectDragIndicator,
+  SelectDragIndicatorWrapper,
+  SelectInput,
+  SelectPortal,
+  SelectTrigger,
+} from "@gluestack-ui/themed";
+import { type ComponentProps, type PropsWithChildren, useState } from "react";
+
+type DenseSelectProps = PropsWithChildren<{
+  onValueChange: ComponentProps<typeof Select>["onValueChange"];
+  selectedValue?: ComponentProps<typeof Select>["selectedValue"];
+  selectedLabel?: ComponentProps<typeof Select>["selectedLabel"];
+  defaultValue?: ComponentProps<typeof Select>["defaultValue"];
+}>;
+
+export default function DenseSelect({
+  onValueChange,
+  selectedLabel,
+  selectedValue,
+  defaultValue,
+  children,
+}: DenseSelectProps) {
+  const [show, setShow] = useState(false);
+
+  function toggleShow() {
+    setShow((prev) => !prev);
+  }
+
+  return (
+    <>
+      <Pressable onPress={toggleShow} h="auto" w="auto" hitSlop={16}>
+        <Icon as={ChevronRightIcon} />
+      </Pressable>
+      <Select
+        onClose={toggleShow}
+        display="none"
+        selectedLabel={selectedLabel}
+        selectedValue={selectedValue}
+        defaultValue={defaultValue}
+        onValueChange={onValueChange}
+      >
+        <SelectTrigger>
+          <SelectInput />
+        </SelectTrigger>
+        <SelectPortal isOpen={show} onClose={toggleShow}>
+          <SelectBackdrop />
+          <SelectContent>
+            <SelectDragIndicatorWrapper>
+              <SelectDragIndicator />
+            </SelectDragIndicatorWrapper>
+            {children}
+          </SelectContent>
+        </SelectPortal>
+      </Select>
+    </>
+  );
+}
