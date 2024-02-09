3c4abdfdd29720a77b13a880d58d63a5f1c87070
William Fernandes Dias <william.winchester1967@gmail.com>
2024-02-09 16:35:51 -0300

    refactor: utilizar tipo "Middleware" do SWR para simplificar definição das funções

--- src/middlewares/historical.ts
+++ src/middlewares/historical.ts
@@ -1,32 +1,30 @@
 import { useEffect, useRef } from "react";
-import type { BareFetcher, Key, SWRHook, SWRResponse } from "swr";
+import type { BareFetcher, Middleware, SWRResponse } from "swr";
 import type { SWRConfiguration } from "swr/_internal";
+
+export type HistoricalSWRResponse<Data, Error> = SWRResponse<
+  Data,
+  Error,
+  SWRConfiguration<Data, Error, BareFetcher<Data>>
+> & { history: Data[] };
 
 /**
  * Middleware para manter um histórico dos dados obtidos por um `SWRHook`.
  */
-export default function historical<Data>(
-  useSWRNext: SWRHook,
-): <Error>(
-  key: Key,
-  fetcher: BareFetcher<Data> | null,
-  config: SWRConfiguration<Data, Error, BareFetcher<Data>>,
-) => SWRResponse<
-  Data,
-  Error,
-  SWRConfiguration<Data, Error, BareFetcher<Data>>
-> & { history: Data[] } {
+const historical: Middleware = (useSWRNext) => {
   return (key, fetcher, config) => {
-    const history = useRef<Data[]>([]);
+    const history = useRef<ReturnType<Exclude<typeof fetcher, null>>[]>([]);
 
     const swr = useSWRNext(key, fetcher, config);
 
     useEffect(() => {
       if (swr.data) {
-        history.current.push(swr.data as Data);
+        history.current.push(swr.data);
       }
     }, [swr.data]);
 
     return Object.assign({}, swr, { history: history.current });
   };
-}
+};
+
+export default historical;
--- src/middlewares/laggy.ts
+++ src/middlewares/laggy.ts
@@ -1,51 +1,35 @@
 import { useCallback, useEffect, useRef } from "react";
-import type {
-  BareFetcher,
-  Key,
-  SWRConfiguration,
-  SWRHook,
-  SWRResponse,
-} from "swr";
+import type { Middleware } from "swr";
 
 /**
  * Middleware para utilizar dados antigos enquanto os novos não são validados.
  */
-export default function laggy<Data>(
-  useSWRNext: SWRHook,
-): <Error>(
-  key: Key,
-  fetcher: BareFetcher<Data> | null,
-  config: SWRConfiguration<Data, Error, BareFetcher<Data>>,
-) => SWRResponse<
-  Data,
-  Error,
-  SWRConfiguration<Data, Error, BareFetcher<Data>>
-> {
-  return (key, fetcher, config) => {
-    const laggyDataRef = useRef<Data>();
+const laggy: Middleware = (useSWRNext) => (key, fetcher, config) => {
+  const laggyDataRef = useRef<ReturnType<Exclude<typeof fetcher, null>>>();
 
-    const swr = useSWRNext(key, fetcher, config);
+  const swr = useSWRNext(key, fetcher, config);
 
-    useEffect(() => {
-      if (swr.data) {
-        laggyDataRef.current = swr.data;
-      }
-    }, [swr.data]);
+  useEffect(() => {
+    if (swr.data) {
+      laggyDataRef.current = swr.data;
+    }
+  }, [swr.data]);
 
-    const resetLaggy = useCallback(() => {
-      laggyDataRef.current = undefined;
-    }, []);
+  const resetLaggy = useCallback(() => {
+    laggyDataRef.current = undefined;
+  }, []);
 
-    const dataOrLaggyData =
-      swr.data === undefined ? laggyDataRef.current : swr.data;
+  const dataOrLaggyData =
+    swr.data === undefined ? laggyDataRef.current : swr.data;
 
-    const isLagging =
-      swr.data === undefined && laggyDataRef.current !== undefined;
+  const isLagging =
+    swr.data === undefined && laggyDataRef.current !== undefined;
 
-    return Object.assign({}, swr, {
-      data: dataOrLaggyData,
-      isLagging,
-      resetLaggy,
-    });
-  };
-}
+  return Object.assign({}, swr, {
+    data: dataOrLaggyData,
+    isLagging,
+    resetLaggy,
+  });
+};
+
+export default laggy;
